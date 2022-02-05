9ce2292ccc287e2eb29771c8eeade3096871442a
Evan-aja <gebel909@student.ub.ac.id>
2022-02-05 20:01:09 +0700

    init commit

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Gabriel Evan',
+    site_url: 'https://evan-aja.vercel.app',
+    feed_url: 'https://evan-aja.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
