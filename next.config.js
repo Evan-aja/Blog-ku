c69a337af4cd4092d3c53f2c72f542ca81887fd2
Evan <71018479+Evan-aja@users.noreply.github.com>
2023-07-25 18:45:20 +0700

    containers

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = {
+  output: 'standalone',
+  basePath: process.env.BASE_URL || '',
+  ...withNextra()
+}
